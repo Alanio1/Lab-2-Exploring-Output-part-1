ad67a6e572ce22ec6b33d2005b5ff3d2cb410ca1
Alanio Reese <reese51@tritons.iowacentral.edu>
2020-09-14 11:44:08 -0500

    Add project files.

---
+++ Lab 2 Exploring Output/Main.cpp
@@ -0,0 +1,60 @@
+//**
+//Alanio
+//Lab 2 Exploring OutPuts
+//8 September 2020
+//**
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string m = "Monday";
+string t = "Tuesday";
+string d = "Wensday";
+string i = "Thrusday";
+string f = "Friday";
+string z = "8:00 AM";
+string o = "11:00 AM";
+string u = "1:00 PM";
+string l = "3:30 PM";
+string n = "C++";
+string k = "6:30";
+string q = "Comp 1";
+string h = "Microeconomics";
+string b = "The collage expierence";
+string s = "   ";
+string v = "Free day/Homework day\n";
+
+int main()
+
+{
+	cout << m;
+	cout << s;
+	cout << o;
+	cout << s;
+	cout << "Comp 1 \n";
+	cout << t;
+	cout << s;
+	cout << z;
+	cout << s;
+	cout << h;
+	cout << s;
+	cout << u;
+	cout << s;
+	cout << "C++\n";
+	cout << d;
+	cout << s;
+	cout << v;
+	cout << i;
+	cout << s;
+	cout << z;
+	cout << s;
+	cout << "Microeconomis\n";
+	cout << f;
+	cout << s;
+	cout << z;
+	cout << s;
+	cout << b;
+	return 0;
+}
